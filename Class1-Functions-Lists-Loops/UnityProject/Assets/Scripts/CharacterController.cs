8c3f5486eb2aa1f791aba980ba717ab78f5bbc00
student <student@bcfe.ie>
2022-11-07 11:59:23 +0000

    Tidied up the code and added comments in

--- Class1-Functions-Lists-Loops/UnityProject/Assets/Scripts/CharacterController.cs
+++ Class1-Functions-Lists-Loops/UnityProject/Assets/Scripts/CharacterController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float desiredSpeed = 0;
+    [SerializeField] private float jumpForce = 5f;
 
-    private float walkSpeed = 5f;
-    private float runSpeed = 10f;
-    private float desiredSpeed = 0;
-    private float jumpForce = 5f;
+    private float cameraXRotation = 0;
+
+    [SerializeField] private float cameraSensitivity = 20f;
+
+    [SerializeField] private Transform cameraTransform;
 
     public Rigidbody characterRb;
 
@@ -22,43 +27,60 @@ public class CharacterController : MonoBehaviour
         MovePlayer();
         DetectWalkRun();
         Jump();
+        HandleCameraRotation();
+    }
+
+    private void HandleCameraRotation()
+    {
+        //We are rotating the transform of the gameObject this script is attached to
+        //We are using the Y axis of the gameObject to rotate on the spot
+        // We are using the mouse's X movement as the input for this
+        this.transform.Rotate(new Vector3(0.0f, Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime, 0.0f));
+
+        //We are doing the same again for the Camera's transform
+        //Only this time we are using the mouse's Y movement as the input
+        cameraTransform.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * -1 * cameraSensitivity * Time.deltaTime, 0.0f, 0.0f));
+        
+        //TODO: Make sure the camera cannot rotate more that 90 degrees in either direction
+        Mathf.Clamp(cameraTransform.rotation.x, -90, 90);
     }
 
     private void DetectWalkRun()
     {
-       // Detect a key being HELD by the user
-        //Input.GetKey
-       // Detct when a users presses a key down once
-        //Input.GetKeyDown
-       // Detect when a user lets go of a key that was pressed
-        //Input.GetKeyUp 
-
-        if(Input.GetKeyDown(KeyCode.V))
+        //We've configured a "Run" input in the Unity Input Manager
+        //If it is pressed, the returned value will be 1
+        //Check if it is great than 0 to detect the press
+        if(Input.GetAxis("Run") > 0)
         {
+            //Set the desiredSpeed to run speed
             desiredSpeed = runSpeed;
         }
 
-        if(Input.GetKeyUp(KeyCode.V))
+        //If it equals zero, the key is not pressed
+        if(Input.GetAxis("Run") == 0)
         {
+            // Set the walk speed
             desiredSpeed = walkSpeed;
         }
-
     }
 
     private void MovePlayer()
     {
-        //If we detect any input move the player
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            characterRb.velocity = new Vector3(
-                    Input.GetAxis("Horizontal") * desiredSpeed,
-                    0.0f,
-                    Input.GetAxis("Vertical") * desiredSpeed);
-        }
+        //We are setting the velocity of the player relative to ITs forward postion
+        //We are then multiplying that by the desiredSpeed
+        //We arethen multiplying that by the Vertical input (-1, 1)
+        characterRb.velocity =
+            this.transform.forward * desiredSpeed * Input.GetAxis("Vertical");
+
+        //Do the same again for left and right but this ADD it to the
+        //Existing velocity so we don't lose forward momentum.
+        characterRb.velocity +=
+            this.transform.right * desiredSpeed * Input.GetAxis("Horizontal");
     }
 
     private void Jump()
     {
+        //TODO: Fix the jump
         if(Input.GetKeyDown(KeyCode.Space))
         {
             characterRb.velocity = new Vector3(0.0f, jumpForce, 0.0f);
